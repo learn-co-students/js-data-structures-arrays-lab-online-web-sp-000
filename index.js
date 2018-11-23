39f6320e08a03ae221332d6b4d9df7f3f9d0bd52
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-11-23 21:07:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield" ];
+
+function destructivelyAppendDriver(name){
+  drivers.push(name);
+  return drivers;
+  }
+  
+function destructivelyPrependDriver(name){
+  drivers.unshift(name);
+  return drivers;
+}
+
+function destructivelyRemoveLastDriver(){
+  drivers.pop(name);
+  return drivers;
+}
+
+function destructivelyRemoveFirstDriver(){
+  drivers.shift(name);
+  return drivers;
+}
+
+ function appendDriver(name){
+   return [...drivers, name]
+ }
+ 
+ function prependDriver(name){
+   return [name, ...drivers]
+ }
+
+function removeLastDriver() {
+  var newDrivers = drivers.slice(0, drivers.length -1)
+  return newDrivers;
+}
+ function removeFirstDriver() {
+  var newDrivers = drivers.slice(1)
+  return newDrivers;
+}
