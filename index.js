14fea5309606d6a79a797f625443ff9750de01e5
ch2isk4kos <ch2isk4kos@gmail.com>
2019-09-12 00:51:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+let drivers = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendDriver(name) {
+    return drivers.push(name);
+}
+
+
+function destructivelyPrependDriver(name) {
+    return drivers.unshift(name);
+}
+
+
+function destructivelyRemoveLastDriver(name) {
+    return drivers.pop(name);
+}
+
+
+function destructivelyRemoveFirstDriver(name) {
+    return drivers.shift(name);
+}
+
+
+function appendDriver(name) {
+    return [...drivers, name];
+}
+
+
+function prependDriver(name) {
+    return [name, ...drivers];
+}
+
+
+function removeFirstDriver() {
+    return drivers.slice(1);
+}
+
+
+function removeLastDriver() {
+    return drivers.slice(0, drivers.length -1);
+}
