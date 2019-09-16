23dc6014a13a626200dd861c563376ae7c2cf234
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-09-16 23:08:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,35 +1,77 @@
 // Write your solution here!
-const drivers = ["Milo", "Otis", "Garfield"];
+// const drivers = ["Milo", "Otis", "Garfield"];
+
+// function destructivelyAppendDriver(name) {
+//   drivers.push(name)
+// }
+
+// function destructivelyPrependDriver(name) {
+//   drivers.unshift(name)
+// }
+
+// function destructivelyRemoveLastDriver() {
+//   drivers.pop()
+// }
+
+// function destructivelyRemoveFirstDriver() {
+//   drivers.shift()
+// }
+
+// function appendDriver(name) {
+//   return [...drivers, name]
+// }
+
+// function prependDriver(name) {
+//   return [name, ...drivers]
+// }
+
+// function removeLastDriver() {
+//   return drivers.slice(0, -1)
+// }
+
+// function removeFirstDriver() {
+//   return drivers.slice(1)
+//   // return drivers.slice(1, drivers.length)
+// }
+const drivers =  ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendDriver(name) {
-  drivers.push(name)
+  drivers.push(name);
 }
 
 function destructivelyPrependDriver(name) {
-  drivers.unshift(name)
+  drivers.unshift(name);
 }
 
 function destructivelyRemoveLastDriver() {
-  drivers.pop()
+  drivers.pop();
 }
 
 function destructivelyRemoveFirstDriver() {
-  drivers.shift()
+  drivers.shift();
 }
 
+
 function appendDriver(name) {
-  return [...drivers, name]
+  drivers2 = drivers.slice();
+  drivers2.push(name);
+  return drivers2;
 }
 
 function prependDriver(name) {
-  return [name, ...drivers]
+  drivers2 = drivers.slice();
+  drivers2.unshift(name);
+  return drivers2;
 }
 
 function removeLastDriver() {
-  return drivers.slice(0, -1)
+  drivers2 = drivers.slice();
+  drivers2.pop();
+  return drivers2;
 }
 
 function removeFirstDriver() {
-  return drivers.slice(1)
-  // return drivers.slice(1, drivers.length)
+  drivers2 = drivers.slice();
+  drivers2.shift();
+  return drivers2;
 }
