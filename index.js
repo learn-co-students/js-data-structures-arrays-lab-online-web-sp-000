30267fce2ef62c6b07598eb649bcc01c31deb943
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-09-16 23:15:37 +0000

    Done.

--- index.js
+++ index.js
@@ -53,25 +53,29 @@ function destructivelyRemoveFirstDriver() {
 
 
 function appendDriver(name) {
-  drivers2 = drivers.slice();
-  drivers2.push(name);
-  return drivers2;
+  // drivers2 = drivers.slice();
+  // drivers2.push(name);
+  // return drivers2;
+  return [...drivers, name];
 }
 
 function prependDriver(name) {
-  drivers2 = drivers.slice();
-  drivers2.unshift(name);
-  return drivers2;
+  // drivers2 = drivers.slice();
+  // drivers2.unshift(name);
+  // return drivers2;
+  return [name, ...drivers];
 }
 
 function removeLastDriver() {
-  drivers2 = drivers.slice();
-  drivers2.pop();
-  return drivers2;
+  // drivers2 = drivers.slice();
+  // drivers2.pop();
+  // return drivers2;
+  return drivers.slice(0, -1);
 }
 
 function removeFirstDriver() {
-  drivers2 = drivers.slice();
-  drivers2.shift();
-  return drivers2;
+  // drivers2 = drivers.slice();
+  // drivers2.shift();
+  // return drivers2;
+  return drivers.slice(1);
 }
